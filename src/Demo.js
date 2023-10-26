4f100c0c35e67bd371a92bfb3346a9c7f20935fc
Anand Singh <35650659+anandsingh111@users.noreply.github.com>
2023-10-26 17:10:45 +0530

    Update Demo.js

--- src/Demo.js
+++ src/Demo.js
@@ -1,92 +1,86 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 
-const SwappableTabs = () => {
-  const [activeTab, setActiveTab] = useState(1);
-  const tabContentRef = useRef(null);
-  const touchStartX = useRef(null);
+const SwipeableTabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(null);
 
   const handleTabChange = (newTab) => {
     setActiveTab(newTab);
   };
 
   const handleTouchStart = (e) => {
-    touchStartX.current = e.touches[0].clientX;
+    setTouchStartX(e.touches[0].clientX);
   };
 
   const handleTouchMove = (e) => {
-    if (touchStartX.current !== null) {
-      e.preventDefault();
+    if (touchStartX !== null) {
+      const deltaX = e.touches[0].clientX - touchStartX;
+      if (deltaX > 50 && activeTab > 0) {
+        handleTabChange(activeTab - 1);
+        setTouchStartX(null);
+      } else if (deltaX < -50 && activeTab < 2) {
+        handleTabChange(activeTab + 1);
+        setTouchStartX(null);
+      }
     }
   };
 
   const handleTouchEnd = () => {
-    if (touchStartX.current !== null) {
-      const tabContentWidth = tabContentRef.current.clientWidth;
-      const touchEndX = touchStartX.current;
-      const deltaX = touchEndX - touchStartX.current;
-
-      if (Math.abs(deltaX) > tabContentWidth / 4) {
-        if (deltaX < 0 && activeTab < 3) {
-          handleTabChange(activeTab + 1);
-        } else if (deltaX > 0 && activeTab > 1) {
-          handleTabChange(activeTab - 1);
-        }
-      }
-
-      touchStartX.current = null;
-    }
+    setTouchStartX(null);
   };
 
   return (
     <div>
       <div className="tab-buttons">
         <button
+          className={activeTab === 0 ? 'active' : ''}
+          onClick={() => handleTabChange(0)}
+        >
+          Tab 1
+        </button>
+        <button
           className={activeTab === 1 ? 'active' : ''}
           onClick={() => handleTabChange(1)}
         >
-          Tab 1
+          Tab 2
         </button>
         <button
           className={activeTab === 2 ? 'active' : ''}
           onClick={() => handleTabChange(2)}
         >
-          Tab 2
-        </button>
-        <button
-          className={activeTab === 3 ? 'active' : ''}
-          onClick={() => handleTabChange(3)}
-        >
           Tab 3
         </button>
       </div>
       <div
-        ref={tabContentRef}
         className="tab-content"
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
       >
-        {activeTab === 1 && (
-          <div>
+        <div
+          style={{
+            transform: `translateX(-${activeTab * 100}%)`,
+            transition: 'transform 0.3s ease',
+            display: 'flex',
+            width: '300%'
+          }}
+        >
+          <div className="tab-pane">
             <h2>Tab 1 Content</h2>
             <p>This is the content for Tab 1.</p>
           </div>
-        )}
-        {activeTab === 2 && (
-          <div>
+          <div className="tab-pane">
             <h2>Tab 2 Content</h2>
             <p>This is the content for Tab 2.</p>
           </div>
-        )}
-        {activeTab === 3 && (
-          <div>
+          <div className="tab-pane">
             <h2>Tab 3 Content</h2>
             <p>This is the content for Tab 3.</p>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
 };
 
-export default SwappableTabs;
+export default SwipeableTabs;
